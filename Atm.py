5ad211b9ab67f43136e4d909d64e22f983f566b4
Suriya N <75084434+snith01-17@users.noreply.github.com>
2022-03-16 20:33:23 -0400

    Add files via upload

---
+++ Atm.py
@@ -0,0 +1,14 @@
+class Atm(object):
+    def __init__(CashWithdrawl, BalanceEnquiry):
+        self.CashWithdrawl = CashWithdrawl
+        self.BalanceEnquiry = BalanceEnquiry
+
+    def cashWithdraw(self):
+        print("You withdrawed $5")    
+
+    def balanceCheck(self):
+        print("You have $0 left!!")    
+
+#Defining random variables
+bankk =   Atm(0, 1)      
+    
